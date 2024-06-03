3a6d574e431504e5582ba9da48f56401c2edf924
harshau007 <amanupadhyay2004@gmail.com>
2024-06-03 23:37:50 +0530

    New Exp

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import About from "@/components/about";
 import Contact from "@/components/contact";
-import Experience from "@/components/experiencenew";
+import Experience from "@/components/experience";
 import Github from "@/components/github";
 import Intro from "@/components/intro";
 import Projects from "@/components/projects";
